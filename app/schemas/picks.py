eb3df830591c2a6a7372e6457e3674535aa35ff4
rober0xf <roberpili02@gmail.com>
2025-05-15 16:19:50 -0300

    picks: http schemas

--- app/schemas/picks.py
+++ app/schemas/picks.py
@@ -1,10 +1,28 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
-from typing import Optional
 
 
-class Picks(BaseModel):
-    id: int = Field(..., gt=0, description='Pick id')
-    fight_id: int = Field(..., gt=0, description='Fight associated')
-    winner_pick: int = Field(..., gt=0, description='Winner picked by the user')
-    created_at: Optional[datetime] = Field(None, description='Set by the datebase (do not provide manually)', validate_default=True)
+class PicksBase(BaseModel):
+    fight_id: int = Field(..., gt=0, description="Fight associated")
+    winner_pick: int = Field(..., gt=0, description="Winner picked by the user")
+
+
+# general model. also response model
+class Picks(PicksBase):
+    id: int = Field(..., gt=0, description="Pick id")
+    created_at: datetime | None = Field(
+        None, description="Set by the datebase (do not provide manually)", validate_default=True
+    )
+
+
+class PicksCreate(PicksBase):
+    pass
+
+
+class PicksUpdate(BaseModel):
+    fight_id: int | None = Field(None, gt=0)
+    winner_pick: int | None = Field(None, gt=0)
+
+
+class PicksPatch(PicksUpdate):
+    pass
