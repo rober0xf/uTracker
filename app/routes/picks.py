fb110555fcd907c5f80b7314356f28d8e94c6e54
rober0xf <robertinopili13@gmail.com>
2025-07-29 16:10:21 -0300

    refactor: picks endpoint

--- app/routes/picks.py
+++ app/routes/picks.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter, status, Depends, HTTPException, Form
+
+from fastapi import APIRouter, Depends, Form, HTTPException, status
 from sqlalchemy.orm import Session
-from typing import List
-from app.schemas.picks import Picks, PicksCreate
-from app.db.models import PicksDB, FightsDB, FightersDB
+
+from app.db.models import FightersDB, FightsDB, PicksDB
 from app.db.session import get_db
+from app.schemas.picks import Picks, PicksCreate
 
 router = APIRouter(prefix="/picks", tags=["Picks"])
 
+db_dependency = Depends(get_db)
+required_form = Form(...)
 
-@router.get("/", response_model=List[Picks], status_code=status.HTTP_200_OK)
-def get_all_picks(db: Session = Depends(get_db)):
+
+@router.get("/", response_model=list[Picks], status_code=status.HTTP_200_OK)
+def get_all_picks(db: Session = db_dependency):
     picks = db.query(PicksDB).all()
     if not picks:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="no picks found")
@@ -17,7 +21,7 @@ def get_all_picks(db: Session = Depends(get_db)):
 
 
 @router.get("/{id}", response_model=Picks, status_code=status.HTTP_200_OK)
-def get_pick(id: int, db: Session = Depends(get_db)):
+def get_pick(id: int, db: Session = db_dependency):
     pick = db.query(PicksDB).filter(PicksDB.id == id).first()
     if not pick:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"pick with id {id} not found")
@@ -25,7 +29,7 @@ def get_pick(id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/", response_model=Picks, status_code=status.HTTP_201_CREATED)
-def create_pick(fight_id: int = Form(...), winner_pick: int = Form(...), db: Session = Depends(get_db)):
+def create_pick(fight_id: int = required_form, winner_pick: int = required_form, db: Session = db_dependency):
     # first we check that the input ids exists
     fight = db.query(FightsDB).filter(FightsDB.id == fight_id).first()
     if not fight:
@@ -38,7 +42,7 @@ def create_pick(fight_id: int = Form(...), winner_pick: int = Form(...), db: Ses
     try:
         pick_data = PicksCreate(fight_id=fight_id, winner_pick=winner_pick)
     except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e))
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e)) from None
 
     new_pick = PicksDB(fight_id=pick_data.fight_id, winner_pick=pick_data.winner_pick)
     db.add(new_pick)
