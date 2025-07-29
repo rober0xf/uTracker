d34bb7d2c48bc7f87f17ba81e59823ef7b4cfadc
rober0xf <robertinopili13@gmail.com>
2025-07-29 16:33:56 -0300

    refactor: database config

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from app.db.settings import settings_database
 
 SQLALCHEMY_DATABASE_URL = settings_database.DATABASE_URL
@@ -7,8 +8,10 @@ SQLALCHEMY_DATABASE_URL = settings_database.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # database session
-SessionLocal = sessionmaker(bind=engine,autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
+
+# get database generator
 def get_db():
     db = SessionLocal()
     try:
--- app/db/settings.py
+++ app/db/settings.py
@@ -1,23 +1,20 @@
-from  pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class DatabaseSettings(BaseSettings):
     DATABASE_USER: str = "postgres"
-    DATABASE_PASSWORD:str = "secret"
+    DATABASE_PASSWORD: str = "secret"
     DATABASE_HOST: str = "localhost"
     DATABASE_PORT: str = "5432"
     DATABASE_NAME: str = "utracker"
 
     @property
     def DATABASE_URL(self) -> str:
-        return (
-            f"postgresql://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}"
-            f"@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
-        )
+        return f"postgresql://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
 
-    model_config = SettingsConfigDict(
-        env_file=  Path(__file__).resolve().parent.parent.parent / ".env",
-        env_file_encoding = "utf-8"
-    )
+    model_config = SettingsConfigDict(env_file=Path(__file__).resolve().parent.parent.parent / ".env", env_file_encoding="utf-8")
+
 
 settings_database = DatabaseSettings()
